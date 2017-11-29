3ca40a435def2b7a2678faa7ab9a47f8170e2b20
Apel-kun <apel.kun@gmail.com>
2017-11-29 20:44:12 +0400

    some fixes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var port = process.env.port || 5000;
+var port = process.env.PORT || 5000;
 var app = express();
 
 app.use(express.static(__dirname, {index: false}));
