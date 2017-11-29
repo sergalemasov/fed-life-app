f43cdda1e19abfb6694b6f6dfce2cab3300cdc70
Apel-kun <apel.kun@gmail.com>
2017-11-29 20:26:32 +0400

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var port = 5000;
+var app = express();
+
+app.use(express.static(__dirname, {index: false}));
+app.get('/', function(request, response) {
+    response.sendfile(__dirname + '/index.html');
+});
+app.listen(port);
